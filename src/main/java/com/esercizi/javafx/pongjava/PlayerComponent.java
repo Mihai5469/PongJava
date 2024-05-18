015fdaa18c269b48deb70625ae0b289f98b16c3f
Mihai5469 <mihaiiachimovschi2@hmail.com>
2024-05-18 22:16:52 +0200

    problema del enemy che andava fuori campo risolto

---
+++ src/main/java/com/esercizi/javafx/pongjava/PlayerComponent.java
@@ -0,0 +1,14 @@
+package com.esercizi.javafx.pongjava;
+
+import com.almasb.fxgl.entity.component.Component;
+import com.almasb.fxgl.entity.components.TransformComponent;
+
+public class PlayerComponent extends Component {
+
+
+    public void print(){
+        System.out.println("SOno un giocatore");
+    }
+
+
+}
